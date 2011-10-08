469024c75ad04f33e25ca7e2ab0e07015ba5641b
Tomas Jansson <tomas.jansson@gmail.com>
2011-10-08 11:32:51 +0200

    added empty donald api

---
+++ DonaldWCFwebApi/APIs/DonaldApi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+namespace DonaldWCFwebApi.APIs
+{
+    [ServiceContract]
+    public class DonaldApi
+    {
+    }
+}
