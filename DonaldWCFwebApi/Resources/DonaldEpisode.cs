72b34ef92ac789e96f320eafbf9da81742105859
Tomas Jansson <tomas.jansson@gmail.com>
2011-10-08 11:37:09 +0200

    added DonaldEpisode resource

---
+++ DonaldWCFwebApi/Resources/DonaldEpisode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DonaldWCFwebApi.Resources
+{
+    public class DonaldEpisode
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string ArtWorkUrl { get; set; }
+        public string Author { get; set; }
+    }
+}
